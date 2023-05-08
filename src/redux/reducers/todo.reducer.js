d17980b38d1633af761aa94bca5f9a3cd84efbfc
arash.esmaeilian <arash.esmaelian@zoodfood.com>
2023-05-08 15:04:39 +0330

    simple local storage service added

--- src/redux/reducers/todo.reducer.js
+++ src/redux/reducers/todo.reducer.js
@@ -1,39 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
+if (!localStorage.getItem("todo")) {
+  localStorage.setItem(
+    "todo",
+    JSON.stringify([
+      {
+        txt: "Complete online javascript course",
+        completed: true,
+        id: 0,
+      },
+      {
+        txt: "Jog around the park",
+        completed: false,
+        id: 1,
+      },
+      {
+        txt: "10 minutes meditation",
+        completed: false,
+        id: 2,
+      },
+      {
+        txt: "Read for one hour",
+        completed: false,
+        id: 3,
+      },
+      {
+        txt: "Pick up grocceries",
+        completed: false,
+        id: 4,
+      },
+      {
+        txt: "Complete todo app on frontend mentor",
+        completed: false,
+        id: 5,
+      },
+    ])
+  );
+}
 
 export const TodoSlice = createSlice({
   name: "todo",
-  initialState: [
-    {
-      txt: "Complete online javascript course",
-      completed: true,
-      id: 0,
-    },
-    {
-      txt: "Jog around the park",
-      completed: false,
-      id: 1,
-    },
-    {
-      txt: "10 minutes meditation",
-      completed: false,
-      id: 2,
-    },
-    {
-      txt: "Read for one hour",
-      completed: false,
-      id: 3,
-    },
-    {
-      txt: "Pick up grocceries",
-      completed: false,
-      id: 4,
-    },
-    {
-      txt: "Complete todo app on frontend mentor",
-      completed: false,
-      id: 5,
-    },
-  ],
+  initialState: JSON.parse(localStorage.getItem("todo")),
   reducers: {
     addTodo: (state, action) => {
       state.push({
@@ -41,17 +47,20 @@ export const TodoSlice = createSlice({
         id: state[state.length - 1].id + 1,
         completed: false,
       });
+      localStorage.setItem("todo", JSON.stringify(state));
     },
     removeTodo: (state, action) => {
       const indexOfRemovingTodo = state.findIndex(
         (todo) => todo.id === action.payload.id
       );
       state.splice(indexOfRemovingTodo, 1);
+      localStorage.setItem("todo", JSON.stringify(state));
     },
     completeTodo: (state, action) => {
       state.forEach((todo) => {
         if (todo.id === action.payload.id) todo.completed = !todo.completed;
       });
+      localStorage.setItem("todo", JSON.stringify(state));
     },
   },
 });
